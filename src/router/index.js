a6cbdd11cb8a63456485798705f1726b12da2206
ImbaCOOL <imbacool@189.cn>
2020-03-07 09:50:53 +0800

    0

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router' // 引入vue路由模块
 // 引入组件
-import recommend from '@/views/Recommend'
-import singer from '@/views/Singer'
-import rank from '@/views/Rank'
-import search from '@/views/Search'
-import my from '@/views/My'
-import SingerDetail from '@/views/Singer/SingerDetail'
+// import recommend from '@/views/Recommend'
+// import singer from '@/views/Singer'
+// import rank from '@/views/Rank'
+// import search from '@/views/Search'
+// import my from '@/views/My'
+// import SingerDetail from '@/views/Singer/SingerDetail'
+
+// 路由懒加载
+const recommend = () => import('@/views/Recommend')
+const singer = () => import('@/views/Singer')
+const rank = () => import('@/views/Rank')
+const search = () => import('@/views/Search')
+const my = () => import('@/views/My')
+const SingerDetail = () => import('@/views/Singer/SingerDetail')
 
 Vue.use(VueRouter) // 使用路由
 
