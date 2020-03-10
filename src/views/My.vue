128ac186615aaf4a2b10ce5bc72a8cb081ce1f46
ImbaCOOL <imbacool@189.cn>
2020-03-10 23:20:55 +0800

    0

--- src/views/My.vue
+++ src/views/My.vue
@@ -1,16 +1,41 @@
 <template>
     <div class="my">
-        <span @click='back'>back</span>
-        <p>功能未开放，敬请期待</p>
+        <div class='top'>
+          <span @click='back' class="iconfont icon--fanhui"></span>
+          <span>我的歌曲收藏列表</span>
+        </div>
+
+        <div class='random' ref="random">
+         <span class="iconfont icon-play-circle"></span>
+         <span>随机播放全部</span>
+       </div>
+
+       <div class="wrapper" ref="wrapper">
+         <ul class="content">
+           <li v-for="(item,index) in List" :key="index">{{item}}
+             <span class="iconfont icon-x-close"></span>
+           </li>
+         </ul>
+       </div>
     </div>
 </template>
 
 <script>
+import BS from 'better-scroll'
 export default {
+  data () {
+    return {
+      List: ['111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333']
+    }
+  },
   methods: {
     back () {
       this.$router.go(-1)
     }
+  },
+  mounted () {
+    const wrapper = this.$refs.wrapper
+    this.bs = new BS(wrapper, { click: true })
   }
 }
 </script>
@@ -24,5 +49,68 @@ export default {
   background: #fff;
   left:0px;
   right: 0px;
+  .top{
+    height: 40px;
+    .w(375);
+    background: @green;
+    position: absolute;
+    top:0;
+    z-index: 3;
+    display: flex;
+    justify-content: center;
+    .icon--fanhui{
+      font-size: 22px;
+      color: @white;
+      position: absolute;
+      left: 20px;
+      top: 6px;
+    }
+    span:last-of-type{
+      font-size: 20px;
+      display: inline-block;
+      margin-top: 5px;
+      color: @white;
+    }
+  }
+  .random{
+    position: absolute;
+    top: 70px;
+    left: 122px;
+    border-radius: 30px;
+    background-color: @green;
+    .w(135);
+    height: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    .icon-play-circle{
+      font-size: 18px;
+    }
+    span:nth-of-type(2){
+      font-size: 14px;
+    }
+  }
+  .wrapper{
+    position: fixed;
+    top: 120px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+    .content{
+      padding: 0 30px;
+      li{
+        font-size: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+        .icon-x-close{
+          font-size: 20px;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/views/search.vue
+++ src/views/search.vue
@@ -1,15 +1,177 @@
 <template>
-  <div>
-    功能未开放，敬请期待
+  <div class="search">
+    <div class="inputBox">
+      <span class="iconfont icon-search"></span>
+      <input type="text" placeholder="搜索歌曲、歌手">
+      <span class="iconfont icon-x-close"></span>
+    </div>
+
+    <div class="wrapper" ref="wrapper">
+      <div class="content">
+        <div class="hot">
+          <p>热门搜索</p>
+          <ul>
+            <li v-for="(item,index) in hotList" :key="index">{{item}}</li>
+          </ul>
+        </div>
+        <div class="history">
+          <p>搜索历史
+          <span class="iconfont icon-trash"></span>
+          </p>
+          <ul>
+            <li v-for="(item,index) in historyList" :key="index">{{item}}
+              <span class="iconfont icon-x-close"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="result-wrapper" ref="resultWrapper">
+      <div class="content">
+        <ul>
+          <li v-for="(item,index) in resultList" :key="index">{{item}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BS from 'better-scroll'
 export default {
-
+  data () {
+    return {
+      hotList: ['我们的歌', '冰雪奇缘2', '张杰', '桥边姑娘', '星辰大海', '哪吒 李宇春', '左手指月', '邓紫棋', '酒醉的蝴蝶', '左肩'],
+      historyList: ['111', '222', '333', '111', '222', '333', '111', '222', '333'],
+      resultList: ['111', '222', '333', '111', '222', '333', '111', '222', '333', '111', '222', '333']
+    }
+  },
+  created () {},
+  mounted () {
+    const wrapper = this.$refs.wrapper
+    const resultWrapper = this.$refs.resultWrapper
+    this.bs = new BS(wrapper, { click: true })
+    this.bs2 = new BS(resultWrapper, { click: true })
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+@import "../style/index.less";
+.search{
+  .inputBox{
+    .w(335);
+    height: 50px;
+    border: 5px solid @green;
+    border-radius: 30px;
+    margin: 20px auto;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    box-sizing: border-box;
+    .icon-search{
+      font-size: 20px;
+      color: @green;
+      margin-right: 5px;
+    }
+    input{
+      .w(280);
+      height: 18px;
+    }
+    .icon-x-close{
+      font-size: 20px;
+      color: @green;
+      margin-left: 5px;
+    }
+  }
+  .hot{
+    .w(335);
+    height: 168px;
+    margin: 0 auto 20px;
+    p{
+      font-size: 14px;
+      margin-bottom: 20px;
+    }
+    ul{
+      height: 134px;
+      li{
+        float: left;
+        background-color: @green;
+        color: #fff;
+        height: 23px;
+        font-size: 14px;
+        box-sizing: border-box;
+        margin: 0 10px 10px 0;
+        padding: 0 10px;
+        border-radius: 10px;
+        line-height: 23px;
+      }
+    }
+  }
+  .history{
+    .w(335);
+    margin: 0 auto 20px;
+    p{
+      font-size: 14px;
+      margin-bottom: 20px;
+      display: flex;
+      align-items: center;
+      position: relative;
+      .icon-trash{
+        font-size: 20px;
+        position: absolute;
+        right: 18px;
+      }
+    }
+    ul{
+      li{
+        color: #000;
+        height: 30px;
+        font-size: 16px;
+        box-sizing: border-box;
+        margin: 0 10px 15px 0;
+        padding: 0 10px;
+        border-radius: 10px;
+        line-height: 30px;
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+  .wrapper{
+    position: fixed;
+    top: 178px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    overflow: hidden;
+  }
+  .result-wrapper{
+    display: none;
+    position: fixed;
+    top: 178px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    overflow: hidden;
+    background-color: #fff;
+    z-index: 2;
+    .content{
+      padding: 0 20px;
+      li{
+        color: #000;
+        height: 30px;
+        font-size: 16px;
+        box-sizing: border-box;
+        margin: 0 10px 15px 0;
+        padding: 0 10px;
+        border-radius: 10px;
+        line-height: 30px;
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+  }
+}
 </style>
