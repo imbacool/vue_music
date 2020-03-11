e370f041b03d2675308b05d962c2a8c0220efce0
ImbaCOOL <imbacool@189.cn>
2020-03-11 15:09:03 +0800

    0

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+// const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin') // gzip 压缩的插件
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i // 要进行压缩的文件类型
+// function resolve (dir) {
+//   return path.join(__dirname, dir)
+// }
 module.exports = {
   productionSourceMap: false, // 把打包后的js.map文件删掉
   publicPath: './', // 解析资源文件用相对路径
@@ -19,6 +25,37 @@ module.exports = {
           '^/aaa': ''
         }
       }
+    }
+  },
+  chainWebpack: (config) => {
+    // 配置路径别名
+    // config.resolve.alias
+    //   .set('@', resolve('./src')) // src的绝对路径起一个别名叫@
+    //   .set('api', resolve('./src/api'))
+    //   .set('style', resolve('./src/style'))
+    //   .set('components', resolve('./src/components'))
+
+    // 配置分析工具
+    // if (process.env.NODE_ENV === 'production') {
+    //   if (process.env.npm_config_report) {
+    //     config
+    //       .plugin('webpack-bundle-analyzer')
+    //       .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+    //       .end()
+    //   }
+    // } else {
+    // }
+  },
+  configureWebpack: config => {
+    // 配置gzip
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(new CompressionPlugin({
+        algorithm: 'gzip',
+        test: productionGzipExtensions,
+        threshold: 10240,
+        minRatio: 0.8
+      })
+      )
     }
   }
 }
